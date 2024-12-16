abaa6a62f6c786db4a528d97b6e5d17a13501592
Jerry Kurian <jerry@genaipeople.com>
2024-12-16 22:25:59 +0530

    Fixed compilation issue

--- src/main/java/com/genaipeople/openai/tool/Function.java
+++ src/main/java/com/genaipeople/openai/tool/Function.java
@@ -1,7 +1,5 @@
 package com.genaipeople.openai.tool;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.genaipeople.openai.tool.type.ObjectType;
